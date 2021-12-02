5ab167d6c196183980c09e841557cac056d48cae
simjanos-dev <simjanos.dev@gmail.com>
2021-12-02 17:43:01 +0100

    Replaced images, home page links and fixex cookie box

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -40,17 +40,17 @@
                 <div class="home-category-description">
                     Válogass a rengeteg előre megtervezett matricáink közül.<br>
                     <ul>
-                        <li><a href="#">Fali matricák</a></li>
-                        <li><a href="#">Autós matricák</a></li>
-                        <li><a href="#">Matrica csomagok</a></li>
+                        <li><a href="/products/100/20000/*/%5B2%5D/%7B%7D/0/1">Fali matricák</a></li>
+                        <li><a href="/products/100/20000/*/%5B1%5D/%7B%7D/0/1">Autós matricák</a></li>
+                        <li><a href="/products/100/20000/*/%5B3%5D/%7B%7D/0/1">Matrica csomagok</a></li>
                     </ul>
                 </div>
-                <a href="#"><button class="button blue"><i class="fa fa-search"></i> Összes matrica</button></a>
+                <a href="/products/100/20000/*/%5B1,2,3%5D/%7B%7D/0/1"><button class="button blue"><i class="fa fa-search"></i> Összes matrica</button></a>
             </div>
         </div>
         <div class="home-category-box col-sm-6 col-xl-3 text-center">
             <div class="home-category">
-                <div class="home-category-image-box"><img src="/images/truck.jpg"></div>
+                <div class="home-category-image-box"><img src="/images/home-custom-logos.jpg"></div>
                 <div class="home-category-name">Egyedi matrica árajánlat</div>
                 <div class="home-category-description">Kérj ajánlatot egyedi matricára a már meglévő mintádból. Remek lehetőség céges logó matricák készítéséhez.</div>
                 <a href="/custom-order"><button class="button blue"><i class="fa fa-comment"></i> Egyedi árajánlat</button></a>
@@ -58,18 +58,18 @@
         </div>
         <div class="home-category-box col-sm-6 col-xl-3 text-center">
             <div class="home-category">
-                <div class="home-category-image-box"><img src="https://www.managementcentre.co.uk/wp-content/uploads/presents-500x500.jpg"></div>
+                <div class="home-category-image-box"><img src="/images/home-ajandekok.jpg"></div>
                 <div class="home-category-name">Ajándéktárgyak</div>
                 <div class="home-category-description"></div>
-                <a href="#"><button class="button blue"><i class="fa fa-gift"></i> Ajándéktárgyak</button></a>
+                <a href="/products/100/20000/*/%5B4%5D/%7B%7D/0/1"><button class="button blue"><i class="fa fa-gift"></i> Ajándéktárgyak</button></a>
             </div>
         </div>
         <div class="home-category-box col-sm-6 col-xl-3 text-center">
             <div class="home-category">
-                <div class="home-category-image-box"><img src="/images/custom-boxes.jpg"></div>
+                <div class="home-category-image-box"><img src="/images/home-papir-diszek.jpg"></div>
                 <div class="home-category-name">Papír ajándékok és díszek</div>
                 <div class="home-category-description">Válogass papírból készült egyedi termékeink közül: díszdobozok, díszborítékok, adventi naptárak. </div>
-                <a href="#"><button class="button blue"><i class="fa fa-sticky-note"></i> Papír díszek</button></a>
+                <a href="/products/100/20000/*/%5B5%5D/%7B%7D/0/1"><button class="button blue"><i class="fa fa-sticky-note"></i> Papír díszek</button></a>
             </div>
         </div>
     </div>
